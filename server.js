e5b836d8769f39840950b91b0a4796b79d532db5
jay-imad <jayendrangs821@gmail.com>
2017-02-27 12:33:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/ui/main.js', function (req, res) {res.sendFile(path.join(__dirname, 'ui', 'main.js'));});
+app.get('/ui/main.js', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 
 var counter = 0;
