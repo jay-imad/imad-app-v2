767dac4446e2dde6420668c90011fe5c04ab56bd
jay-imad <jayendrangs821@gmail.com>
2017-02-27 12:53:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 
-var counter = 0;
+//var counter = 0;
 app.get('/counter', function(req,res){
     counter = counter + 1;
     res.send(counter.toString());
