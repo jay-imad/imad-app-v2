d52f6ab47cda7d015a2a47a54e55c29386293c13
jay-imad <jayendrangs821@gmail.com>
2017-03-01 19:04:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/articles/:articleName', function(req,res) {
     var articleName = req.params.articleName;
     
     var articleData = 
-    pool.query("SELECT * FROM article WHERE TITLE =" + req.params.articleName, function(err, result){
+    pool.query("SELECT * FROM article WHERE TITLE = '" + req.params.articleName,"'", function(err, result){
         if(err) {
             res.status(500).send(err.toString());
         } else{
