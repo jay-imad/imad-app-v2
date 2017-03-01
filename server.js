ca4ae9fd9e7bc1edc3d81142a20a2e987e13c35c
jay-imad <jayendrangs821@gmail.com>
2017-03-01 18:41:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,8 +95,8 @@ app.get('/test-db', function(req,res){
     //make a select request
     //return a response with the results
     pool.query('SELECT * FROM test', function(err,result){
-        if(err){
-            res.status(500).send(err,toString());
+        if(err) {
+            res.status(500).send(err.toString());
         } else{
             res.send(JSON.stringify(result));
         }
