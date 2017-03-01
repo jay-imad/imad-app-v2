81f98ef202ce6cfa57fdaadeb125b660c1a46623
jay-imad <jayendrangs821@gmail.com>
2017-03-01 18:35:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool =  require('pg').Pool;
+
+var config = {
+  host: 'http://db.imad.hasura-app.io/',
+  user: 'jay-imad',
+  password: process.env.DB_PASSWORD,
+  database: 'jay-imad',
+  port: '5432'
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -79,6 +88,19 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test', function(err,result){
+        if(err){
+            res.status(500).send(err,toString());
+        } else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 app.get('/:articleName', function(req,res) {
