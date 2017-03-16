c42095ab69ef2acd9b1ccd41ae14bb152eaa20f4
jay-imad <jayendrangs821@gmail.com>
2017-03-16 19:19:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.post('/login', function (req, res) {
                 res.send('credentials correct!');
                 
               } else {
-                res.status(403).send('username/password is invalid');
+                res.status(403).json('username/password is invalid');
               }
           }
       }
